ae50e1d1bf45d2c45475b4546af20ed33a9495e1
Dimitri Lucas <dimitri.lucas@gfi.fr>
2016-01-27 17:30:27 +0100

    Ajout nouvelles varibles pour les roles + ajout commentaires + modif de la constitution de la liste des modules

--- mapfishapp/src/main/webapp/app/js/GEOR_wmcbrowser.js
+++ mapfishapp/src/main/webapp/app/js/GEOR_wmcbrowser.js
@@ -426,14 +426,18 @@ GEOR.wmcbrowser = (function() {
      * Parameters:
      * storeData - {Array} The list of contexts to filter
      */
-    var filterContexts = function(storeData) {
+    var filterContexts = function(storeData) {//Nouvelle méthode de filtre des contextes
 	var filteredStoreData = [];
-	Ext.each(storeData, function(data) {
+	Ext.each(storeData, function(data) { //Pour chaque contexte
+		//On doit récupèrer tous les contextes defaut 
+		//(c'est à dire tous les fichiers qui commence par default)
 		if( data.title.slice(0, "default".length) === "default") {
 			filteredStoreData.push(data);
 			return true;
 		} else {
-			Ext.each(GEOR.config.ROLES, function(role) {
+			//On regarde si l'utilisateur a les droits d'accès aux contextes
+			//les fichiers wmc doit commencer par le nom du role
+			Ext.each(GEOR.config.GEOBUILDER_ROLES, function(role) {
 				if (data.title.slice(0, role.length) === role) {
 					filteredStoreData.push(data);
 					return true;
--- mapfishapp/src/main/webapp/app/js/geobuilder/GEOR_geobuilder_module_metier.js
+++ mapfishapp/src/main/webapp/app/js/geobuilder/GEOR_geobuilder_module_metier.js
@@ -1,9 +1,9 @@
 Ext.namespace("GEOR");
 
 GEOR.geobuilder_initListeModule = function (mapPanel) {
-	//Creation du menu geobuilder
+	//Creation de combobox de la liste des module métier
 	
-	//Récupération du menu JSon
+	//Récupération de la liste total des modules en JSon
     var url = GEOR.config.GEOBUILDER_URL + GEOR.config.GEOBUILDER_CFM_PROFILES;
 	Ext.Ajax.request
     ({
@@ -16,74 +16,65 @@ GEOR.geobuilder_initListeModule = function (mapPanel) {
         		
 	            try{
 		            var data = JSON.parse(response.responseText);
-		            
-		            if (data && data.data && data.data.pros && data.data.pros.length > 0) {
-			            //On cherche la position ou l'on doit insérer ce menu
-			            var listeItems = mapPanel.toolbars[0].items;
+		            //On constitue la liste des modules si l'utilisateur à au moins 1 profil
+		            if (data && data.data && data.data.pros && data.data.pros.length > 0
+		            		&& GEOR.config.GEOBUILDER_ROLES.length > 0) {
+		            	
+			            /*
+			             *
+			             * Constitution de la liste des modules
+			             * 
+			             */
 			            
-			            var position;
-			            for (var i = 0; i < listeItems.length; i++) {
-			            	if (listeItems.items[i].text === tr('Tools')) {
-			            		position = i;
-			            		break;
-			            	}
-			            }
-			            
-			           var moduleDropDown = [{
+			            //On ajoute le module de base
+			            var moduleDropDown = [{
 	            			id: 'default',
 	            			name: tr("Module de base")			
-			           }];
-
-			            //Pour chaque menu geobuilder, on l'ajoute à la toolbar
-			            //A partir du certain nombre de menu, on crée un menu Dropdown pour mettre la suite des menus Geobuilder
-	            		for (var i = 0; i < data.data.pros.length; i++) {
-			            	var id = GEOR.config.GEOBUILDER_GROUPE_LDAP + data.data.pros[i].id;
-			            	var name = data.data.pros[i].name;
-			            	moduleDropDown[i+1] = {
-		            			id: id,
-		            			name: name             			
-		            		};
-			            }
+			            }];
+			            
+			            //Pour chaque role de l'utilisateur, on récupère le nom du profil
+			            var nbProfil = 1;
+	            		Ext.each(GEOR.config.GEOBUILDER_ROLES, function(role) {
+	            			Ext.each(data.data.pros, function(profil) {
+				            	var idProfil = GEOR.config.GEOBUILDER_GROUPE_LDAP + profil.id;
+	            				if (idProfil === role) {
+					            	var name = profil.name;
+					            	moduleDropDown[nbProfil] = {
+				            			id: idProfil,
+				            			name: profil.name             			
+				            		};
+					            	nbProfil++;
+					            	return;
+	            				}
+	            			});
+	            		});
 	            		
-	            		moduleDropDown[data.data.pros.length+1] = {
-		            			id: "ROLE_EL_TEST",
-		            			name: "metier test"             			
-		            		};
+	            		//TODO a enlever pour test
+	            		moduleDropDown[nbProfil] = {
+	            			id: "ROLE_EL_TEST",
+	            			name: "metier test"             			
+	            		};//FIN TODO a enlever pour test
 	            		
+	            		//Creation du store pour la combo
 	            		var storeCombo = new Ext.data.ArrayStore({
             		        autoDestroy: true,
             		        fields: ['id', 'name'],
             		        data : moduleDropDown
             		    });
-	            		
+	            		//Bug extjs, on doit remplir le chams data des items 
+	            		//du store pour afficher les libellés de la combo
 	            		for (var i = 0; i < storeCombo.totalLength; i++) {
 	            			storeCombo.data.items[i].data = storeCombo.data.items[i].json;
 	            		}
 	            		
-	            		var searchContext = function(storeData, moduleMetier) {
-	            			var fileContext = undefined;
-	            			
-	            			Ext.each(storeData, function(data) {
-            					Ext.each(GEOR.config.ROLES, function(role) {
-            						if (role === moduleMetier && data.title === role ) {
-            							fileContext = data.wmc;
-            							return;
-            						}
-            				    });
-	            			});
-	            			if (fileContext === undefined) {
-		            			Ext.each(storeData, function(data) {
-            						if (data.title === "default" ) {
-            							fileContext = data.wmc; 
-            						}
-            				    });
-	            			}
-	            			return fileContext;
-	            		}
-	            		
+	            		/*
+	            		 * On selectionne le module de base par defaut
+	            		 * sauf si on a passé en paramètre le module à charger
+	            		 */
 	            		var module = 'default';
-	            		if (GEOR.config.CUSTOM_MODULE) {		       
-        					Ext.each(GEOR.config.ROLES, function(role) {
+	            		if (GEOR.config.CUSTOM_MODULE) {
+	            			//On vérifie si l'utilisateur à bien les droits
+        					Ext.each(GEOR.config.GEOBUILDER_ROLES, function(role) {
         						if (role === GEOR.config.CUSTOM_MODULE ) {
         							module = GEOR.config.CUSTOM_MODULE;
         							return;
@@ -91,6 +82,35 @@ GEOR.geobuilder_initListeModule = function (mapPanel) {
         				    });
 	            		}
 	            		
+	            		
+	            		/*
+	            		 * Methodes pour la combo
+	            		 */
+	            		//Methode de recherche du contexte selon le module métier sélectionné
+	            		var searchContext = function(moduleMetier) {
+	            			var fileContext = undefined;
+	            			
+	            			//On cherche le contexte associé au module selectionné 
+	            			//Le nom du fichier est le nom du profil
+	            			Ext.each(GEOR.config.CONTEXTS, function(context) {
+        						if (context.title === moduleMetier) {
+        							fileContext = context.wmc;
+        							return;
+        						}
+	            			});
+							//Si il n'y a pas de fichier correspondont, on prend le context par defaut
+	            			if (fileContext === undefined) {
+		            			Ext.each(storeData, function(data) {
+            						if (data.title === "default" ) {
+            							fileContext = data.wmc; 
+            							return;
+            						}
+            				    });
+	            			}
+	            			return fileContext;
+	            		}
+	            		
+	            		//Méthode en cas d'erreur du chargement du contexte
 	            		var onFailure = function(msg) {
 	            	        GEOR.util.errorDialog({
 	            	            msg: tr(msg)
@@ -98,7 +118,7 @@ GEOR.geobuilder_initListeModule = function (mapPanel) {
 	            	        GEOR.waiter.hide();
 	            	    };
 	            		
-	            	    
+	            	    //Creation de la combobox
 	            		var combo = new Ext.form.ComboBox({
 	            		    store: storeCombo,
 	            		    displayField: 'name', 
@@ -113,24 +133,27 @@ GEOR.geobuilder_initListeModule = function (mapPanel) {
 	                            "select": function(combo, record) {
 		            		    	 GEOR.waiter.show();
 		            		    	 
-		            		    	 var fileContext = searchContext(GEOR.config.CONTEXTS, record.data.id);
+		            		    	//Récupération du fichier wmc
+		            		    	 var fileContext = searchContext(record.data.id);
 		            		    	 
+		            		    	 //Récupération du flux wmc
 		            		         OpenLayers.Request.GET({
 		            		             url: GEOR.util.getValidURI(fileContext),
 		            		             success: function(response) {
 		            		                 GEOR.waiter.hide();
+		            		                 //Mise à jour et recharge du nouveau contexte
 		            		                 GEOR.wmc.read(response.responseXML, true, true);
 		            		             },
 		            		             failure: onFailure.createCallback("Could not find WMC file")
 		            		         });
 	                            },
-	                            'expand'   : function(combo) {
+	                            'expand'   : function(combo) { //Méthode pour enlever le curseur dans la combo
 	                                var blurField = function(el) {
 	                                    el.blur();
 	                                }
 	                                blurField.defer(10,this,[combo.el]);
 	                            },
-	                            'collapse'   : function(combo) {
+	                            'collapse'   : function(combo) {//Méthode pour enlever le curseur dans la combo
 	                                var blurField = function(el) {
 	                                    el.blur();
 	                                }
@@ -141,6 +164,15 @@ GEOR.geobuilder_initListeModule = function (mapPanel) {
 	            		    iconCls: 'no-icon' //use iconCls if placing within menu to shift to right side of menu
 	            		});
 	            		
+	            		//On cherche la position ou l'on doit insérer ce menu (Avant le menu Outils)
+			            var position;
+		            	var listeItems = mapPanel.toolbars[0].items;			 
+			            for (var i = 0; i < listeItems.length; i++) {
+			            	if (listeItems.items[i].text === tr('Tools')) {
+			            		position = i;
+			            		break;
+			            	}
+			            }			            
 	            		mapPanel.getTopToolbar().insertButton(position, combo);
 			            		            
 			            /**
@@ -150,6 +182,7 @@ GEOR.geobuilder_initListeModule = function (mapPanel) {
 		            }
 		        }catch(e){
 		        	//error in the above string(in this case,yes)!
+		        	console.error(e);
 	                console.error(response.responseText);
 	            }
         	} else {
