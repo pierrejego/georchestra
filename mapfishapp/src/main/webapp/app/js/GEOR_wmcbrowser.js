e17bcf6860d6d67bb82d255f6afe4f9fabb84d76
Dimitri Lucas <dimitri.lucas@gfi.fr>
2016-01-28 15:41:03 +0100

    Ajout commentaire dans la methode de filtre des contextes

--- mapfishapp/src/main/webapp/app/js/GEOR_wmcbrowser.js
+++ mapfishapp/src/main/webapp/app/js/GEOR_wmcbrowser.js
@@ -426,33 +426,37 @@ GEOR.wmcbrowser = (function() {
      * Parameters:
      * storeData - {Array} The list of contexts to filter
      */
-    var filterContexts = function(storeData) {
-	var filteredStoreData = [];
-	Ext.each(storeData, function(data) {
-		if( data.title.slice(0, "default".length) === "default") {
-			filteredStoreData.push(data);
-		} else {
-			Ext.each(GEOR.config.ROLES, function(role) {
-				if (data.title.slice(0, role.length) === role) {
-					filteredStoreData.push(data);
-					return true;
-				}
-		    });
-		 }		
+    var filterContexts = function(storeData) {//Nouvelle méthode de filtre des contextes
+		var filteredStoreData = [];
+		Ext.each(storeData, function(data) { //Pour chaque contexte
+			//On doit récupèrer tous les contextes defaut 
+			//(c'est à dire tous les fichiers qui commence par default)
+			if( data.title.slice(0, "default".length) === "default") {
+				filteredStoreData.push(data);
+			} else {
+				//On regarde si l'utilisateur a les droits d'accès aux contextes
+				//les fichiers wmc doit commencer par le nom du role
+				Ext.each(GEOR.config.ROLES, function(role) {
+					if (data.title.slice(0, role.length) === role) {
+						filteredStoreData.push(data);
+						return true;
+					}
+			    });
+			 }		
 		
-		// Old filter contexts
-	    /*if( data.roles == undefined || data.roles.length === 0 ) {
-	    	filteredStoreData.push(data);
-	    	return true;
-	    }
-	    Ext.each(data.roles, function(role) {
-		if (GEOR.config.ROLES.indexOf(role) >= 0) {
-		    filteredStoreData.push(data);
-		    return false;
-		}
-	    });*/
-	});
-	return filteredStoreData;
+			// Old filter contexts
+		    /*if( data.roles == undefined || data.roles.length === 0 ) {
+		    	filteredStoreData.push(data);
+		    	return true;
+		    }
+		    Ext.each(data.roles, function(role) {
+			if (GEOR.config.ROLES.indexOf(role) >= 0) {
+			    filteredStoreData.push(data);
+			    return false;
+			}
+		    });*/
+		});
+		return filteredStoreData;
     }
 
     /*
