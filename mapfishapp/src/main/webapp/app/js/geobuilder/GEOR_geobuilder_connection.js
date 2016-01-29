4021d0cf486776761f732389f6a0b58e7039854d
Dimitri Lucas <dimitri.lucas@gfi.fr>
2016-01-29 11:30:54 +0100

    Ajout en commentaire de la nouvelle methode de connexion à geobuilder a appeler

--- mapfishapp/src/main/webapp/app/js/geobuilder/GEOR_geobuilder_connection.js
+++ mapfishapp/src/main/webapp/app/js/geobuilder/GEOR_geobuilder_connection.js
@@ -1,5 +1,6 @@
 Ext.namespace("GEOR");
 
+//TODO a supprimer lorsque la nouvelle gestion du loin par header sera mise en place par geobuilder
 GEOR.geobuilder_connection = function (mapPanel, idProfil, isReconnect) {
 	// Connect to geobuilder server and create session + token for authentication
     Ext.Ajax.request
@@ -70,6 +71,54 @@ GEOR.geobuilder_connection = function (mapPanel, idProfil, isReconnect) {
             return false;
         }
     });
-
 	
 };
+
+//TODO a mettre à place de l'autre méthode lorsque la nouvelle gestion du loin par header sera mise en place par geobuilder
+/*GEOR.geobuilder_connection = function (mapPanel, idProfil, isReconnect) {
+	// Connect to geobuilder server and create session 
+    var url = GEOR.config.GEOBUILDER_URL + GEOR.config.GEOBUILDER_CFM_LOGIN;
+	
+	var params = {};
+	
+	if (idProfil) {  
+		//On passe unqiuement le trigramme a geobuilder
+		var profils = idProfil.split(GEOR.config.GEOBUILDER_GROUPE_LDAP);
+		if (profils.length === 2) {
+			params.propro = profils[1];
+		}
+	}
+	
+    Ext.Ajax.request
+    ({
+        url: url,
+        method: 'GET',
+        params: params,
+        success: function (response) {
+        	
+        	//Si on se reconnecte, il faut supprimer le menu geobuilder et le tab panel geobuilder
+        	if (isReconnect) {
+        		GEOR.geobuilder_destroyMenu(mapPanel);
+        		GEOR.geobuilder_destroyGeobuilderTab();
+        	}
+        	
+            // Appel de la fonction d'init du menu geobuilder dans la toolbar
+            GEOR.geobuilder_initMenu(mapPanel);
+            
+            //Dans le cas d'une reconnexion, on ne rappelle pas la liste des modules et le chargement des iframes
+            if (!isReconnect) {
+                // Appel de la fonction d'init de la liste des modules geobuilder dans la toolbar
+                GEOR.geobuilder_initListeModule(mapPanel);
+
+            	// Load all 5 IFRAMEs for geobuilder to use
+            	GEOR.geobuilder_loadIFRAMES();
+            }
+        	return true;
+        },
+        failure: function (response) {
+        	console.log(response.responseText);
+        	return false;
+        }
+    });
+	
+};*/
