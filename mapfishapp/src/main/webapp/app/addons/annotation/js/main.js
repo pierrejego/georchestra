6800b9a7892df966dec01dcbba6ded13378b5d8a
Jérémy Le Piolet <jeremy.le-piolet@gfi.fr>
2016-04-18 16:45:58 +0200

    Aggrandissememnt de la taille de la pop-up des outils de dessin

--- mapfishapp/src/main/webapp/app/addons/annotation/js/main.js
+++ mapfishapp/src/main/webapp/app/addons/annotation/js/main.js
@@ -17,7 +17,7 @@ GEOR.Addons.Annotation = Ext.extend(GEOR.Addons.Base, {
         });
         this.window = new Ext.Window({
             title: OpenLayers.i18n('annotation.drawing_tools'),
-            width: 440,
+            width: 510,
             closable: true,
             closeAction: "hide",
             resizable: false,
