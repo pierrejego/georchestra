0be3164bd6f2130ded1cee49a43d92061b29210f
pierre jego <pierre.jego@gfi.fr>
2016-06-16 17:11:47 +0200

    Add text font to combox

--- mapfishapp/src/main/webapp/app/js/GEOR_geobuilder_module_metier.js
+++ mapfishapp/src/main/webapp/app/js/GEOR_geobuilder_module_metier.js
@@ -82,7 +82,7 @@ GEOR.geobuilder_initListeModule = function (mapPanel) {
 		            				var contextTitle = context.title;
 		            				var defautContext = "default";
 		            				// Le WMC doit doit etre nommé ROLE_SV_PWRS_xxxx_'moduleMetier'_default.wmc
-		            				if(ccontextTitle && 
+		            				if(contextTitle && 
 		            						(contextTitle.indexOf(GEOR.config.GEOBUILDER_GROUPE_LDAP) > -1) && 
 		            						(contextTitle.indexOf(moduleMetier) > -1) &&
 		            						(contextTitle.indexOf(defautContext) > -1)){
@@ -113,8 +113,7 @@ GEOR.geobuilder_initListeModule = function (mapPanel) {
 		            	    //Creation de la combobox
 		            		var combo = new Ext.form.ComboBox({
 		            		    store: storeCombo,
-		            		    listWidth:70,
-		            		    width:70,
+		            		    width:100,
 		            		    cls: 'comboModuleMetier',
 		            		    displayField: 'name', 
 		            		    valueField: 'id',
